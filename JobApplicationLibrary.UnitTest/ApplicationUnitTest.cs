b0877be754daae7e74ed9a8a54d6bff598e9fcf1
burak <burak@DESKTOP-02M7DF2>
2025-05-10 19:05:52 +0300

    Add project files.

---
+++ JobApplicationLibrary.UnitTest/ApplicationUnitTest.cs
@@ -0,0 +1,48 @@
+﻿using UnitTestDeneme;
+using UnitTestDeneme.Models;
+using NUnit.Framework;
+
+namespace JobApplicationLibrary.UnitTest
+{
+    public class ApplicationUnitTest
+    {
+        
+
+        
+
+        [Test]
+        public void FunctionUnderTest_ExpectedResult_UnderCondition()
+        {
+            // Arrange
+            var evulator = new ApplicationEva();
+            var app = new JobApp()
+            {
+                personel = new Personel()
+                {
+                    Age = 19
+                }
+            };
+
+            // Act
+
+            var appresult = evulator.Evaluate(app);
+
+            // Assert
+            
+            if (appresult == AppResult.AutoAccepted)
+            {
+                // Koşul sağlandı, test başarılı olarak işaretlenebilir
+                Assert.Pass("Beklenen sonuç  elde edildi.");
+            }
+            else
+            {
+                // Koşul sağlanmadı, test başarısız olarak işaretlenmeli
+                Assert.Fail($"{appresult} elde edildi.");
+            }
+        }
+
+
+    }
+    
+    
+}
---
+++ UnitTestDeneme/ApplicationEva.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestDeneme.Models;
+
+namespace UnitTestDeneme
+{
+    public class ApplicationEva
+    {
+        private const int minAge = 18;
+        public AppResult Evaluate(JobApp jobApp)
+        {
+            if (jobApp.personel.Age < minAge)
+                return AppResult.AutoRejected;
+
+            return AppResult.AutoAccepted;
+        }
+    }
+}
+
+public enum AppResult
+{
+    AutoAccepted,
+    TransferredHR,
+    TransferredToLead,
+    TransferredCTO,
+    AutoRejected,
+}
